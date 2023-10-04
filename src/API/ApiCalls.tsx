2a2702bc569badef0c7166ec609689d291098215
Michael <moreno.m2698@gmail.com>
2023-10-04 17:58:55 -0400

    updated format in track api calls

--- src/API/ApiCalls.tsx
+++ src/API/ApiCalls.tsx
@@ -2,103 +2,102 @@ import axios from "axios";
 import { Track } from "../types";
 import { getAlbumImage } from "./AlbumAPICalls";
 
-  const singleEndpoint = process.env.VITE_SINGLE_ENDPOINT;
-  const trackEndpoint = process.env.VITE_TRACK_ENDPOINT;
+interface getTracksResponse {
+  errorState: boolean
+  tracks: Track[]
+}
 
-  export const getTrackAudio = async (trackID: number) => {
-    try {
-      const audioResponse = await axios.get(`${trackEndpoint}/play/${trackID}`, { responseType: "blob" });
-      let audioBlob = new Blob([audioResponse.data], { type: "audio/mpeg" });
-      const audioURL = URL.createObjectURL(audioBlob);
-      return audioURL;
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
-  };
+const singleEndpoint = process.env.VITE_SINGLE_ENDPOINT;
+const trackEndpoint = process.env.VITE_TRACK_ENDPOINT;
+
+export async function getTrackAudio(trackID: number) {
+  try {
+    const audioResponse = await axios.get(`${trackEndpoint}/play/${trackID}`, { responseType: "blob" });
+    let audioBlob = new Blob([audioResponse.data], { type: "audio/mpeg" });
+    const audioURL = URL.createObjectURL(audioBlob);
+    return audioURL;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+async function getSingleImage(singleID:number) {
+  try {
+    const imageResponse = await axios.get(`${singleEndpoint}/image/${singleID}`, { responseType: "blob" });
+    let imageBlob = new Blob([imageResponse.data], { type: "image/png" });
+    const imageURL = URL.createObjectURL(imageBlob);
+    return imageURL;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+async function getSingleAudio(singleId: number) {
+  try {
+    const audioResponse = await axios.get(`${singleEndpoint}/play/${singleId}`, { responseType: "blob" });
+    let audioBlob = new Blob([audioResponse.data], { type: "audio/mpeg" });
+    const audioURL = URL.createObjectURL(audioBlob);
+    return audioURL;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 
-  const getSingleImage = async (singleID: number) => {
-    try {
-      const imageResponse = await axios.get(`${singleEndpoint}/image/${singleID}`, { responseType: "blob" });
-      let imageBlob = new Blob([imageResponse.data], { type: "image/png" });
-      const imageURL = URL.createObjectURL(imageBlob);
-      return imageURL;
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
-  };
+async function getSingle() {
+try {
+  if (singleEndpoint !== undefined) {
+  const singleResponse = await axios.get(singleEndpoint);
+  
+  for (let i = 0; i < singleResponse.data.length; i++) {
 
-  const getSingleAudio = async (singleId: number) => {
-    try {
-      const audioResponse = await axios.get(`${singleEndpoint}/play/${singleId}`, { responseType: "blob" });
-      let audioBlob = new Blob([audioResponse.data], { type: "audio/mpeg" });
-      const audioURL = URL.createObjectURL(audioBlob);
-      return audioURL;
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
-    
+    const single = singleResponse.data[i]
+    single.image = await getSingleImage(single.id)
+    single.audio = await getSingleAudio(single.id)
+
   }
 
-const getSingle = async () => {
-  try {
-    if (singleEndpoint !== undefined) {
-    const singleResponse = await axios.get(singleEndpoint);
-    
-    for (let i = 0; i < singleResponse.data.length; i++) {
-      const single = singleResponse.data[i]
-
-      single.image = await getSingleImage(single.id)
-      single.audio = await getSingleAudio(single.id)
-
-    }
-
-    return singleResponse.data
-    }
-
+  return singleResponse.data}
   } catch (error) {
     console.error("There was an issue access singles", error);
     return []
   }
 }
-  interface getTracksResponse {
-    errorState: boolean
-    tracks: Track[]
-    }
 
 
 
-export const getTracks = async () => {
-    try {
-      if (trackEndpoint !== undefined) {
-        const trackResponse = await axios.get(trackEndpoint)
 
-        for (let i=0; i < trackResponse.data.length; i++) {
+export async function getTracks() {
+  try {
+    if (trackEndpoint !== undefined) {
+      const trackResponse = await axios.get(trackEndpoint)
 
-            const track = trackResponse.data[i];
-            track.image = await getAlbumImage(track.albumId);
-            track.audio = await getTrackAudio(track.id);
-        }
+      for (let i=0; i < trackResponse.data.length; i++) {
 
-        const singleResponse = await getSingle();
-
-        
-        const result: getTracksResponse = {
-            errorState: false,
-            tracks: [...trackResponse.data, ...singleResponse]
-        }
-        return result;
+          const track = trackResponse.data[i];
+          track.image = await getAlbumImage(track.albumId);
+          track.audio = await getTrackAudio(track.id);
       }
-    } catch (error) {
-        console.log(error); 
-        const result: getTracksResponse = {
-            errorState: true,
-            tracks: []
-        }
+
+      const singleResponse = await getSingle();
+
+      
+      const result: getTracksResponse = {
+          errorState: false,
+          tracks: [...trackResponse.data, ...singleResponse]
+      }
       return result;
     }
+  } catch (error) {
+      console.log(error); 
+      const result: getTracksResponse = {
+          errorState: true,
+          tracks: []
+      }
+    return result;
+  }
 }
 
