e6911f76e682b1fa3194893096d86730fc215083
Michael <moreno.m2698@gmail.com>
2023-10-04 18:24:07 -0400

    implemented react-query

--- src/components/MusicSideBarComponents/MusicSideBar.tsx
+++ src/components/MusicSideBarComponents/MusicSideBar.tsx
@@ -12,11 +12,11 @@ function MusicSideBar() {
 
   const trackQuery = useQuery({
     queryKey: ["tracks"],
-    queryFn: getTracks()
+    queryFn: trackResponseHandler()
   })
 
-  useEffect(() => {
-    (async() => {
+  async function trackResponseHandler() {
+    try {
       if (tracks.length === 0 && setTracks !== undefined && setErrorState !== undefined && setPlayerTracks !== undefined) {
         const response = await getTracks();
         if (response !== undefined) {
@@ -31,14 +31,46 @@ function MusicSideBar() {
           setErrorState(errorState)
         }
       }
-    })();
-  },[])
+    } catch (err) {
+      console.log("There was an error in getting track response: ", err)
+    }
+  }
 
-  return (
-    <ul className="sidebar__content outer">
-        {(trackQuery.isLoading) ? "...Loading" : tracks.map((track:Track, index) => <MusicTile track={track} key={index} trackIndex={index}/>)}
-    </ul>
-  )
+  // useEffect(() => {
+  //   (async() => {
+  //     if (tracks.length === 0 && setTracks !== undefined && setErrorState !== undefined && setPlayerTracks !== undefined) {
+  //       const response = await getTracks();
+  //       if (response !== undefined) {
+  //         const tracks = response.tracks
+  //         const errorState = response.errorState
+  //         const trackQueue = {
+  //           id: "tracks",
+  //           tracks: tracks
+  //         }
+  //         setPlayerTracks(trackQueue)
+  //         setTracks(tracks)
+  //         setErrorState(errorState)
+  //       }
+  //     }
+  //   })();
+  // },[])
+
+  if (trackQuery.isLoading) 
+    return (
+      <ul className="sidebar__content outer">
+        <p>"...Loading"</p>
+      </ul>
+    )
+  if (trackQuery.isError) 
+    return (
+      <pre>{JSON.stringify(trackQuery.error)}</pre>
+    )
+  else 
+    return (
+      <ul className="sidebar__content outer">
+        {tracks.map((track:Track, index) => <MusicTile track={track} key={index} trackIndex={index}/>)}
+      </ul>
+    )
 }
 
 export default MusicSideBar
